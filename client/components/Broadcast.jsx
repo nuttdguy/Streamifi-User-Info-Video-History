4c25c24d764cadc181c9d6a0ece3224edc7d34ce
tomjimseng <tomjimseng@gmail.com>
2020-03-11 23:39:42 -0700

    Test for pull request

--- client/components/Broadcast.jsx
+++ client/components/Broadcast.jsx
@@ -2,4 +2,14 @@ import React from "react";
 
 const Message = console.log('Hello World');
 
+class Broadcast extends React.Component {
+    components(props){
+        super(props);
+
+    }
+    render() {
+        return(console.log('Testing Pull Request'));
+    }
+}
+
 export default Broadcast;
