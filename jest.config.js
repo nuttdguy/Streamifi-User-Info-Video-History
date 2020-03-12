03059fcfd3bebe31dc5c442558b06b3b25ed317b
tomjimseng <tomjimseng@gmail.com>
2020-03-11 22:59:55 -0700

    created jest config document

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const {defaults} = require('jest-config');
+
+module.exports = {
+    verbose: true,
+    clearMocks: true,
+    collectCoverage: true,
+    moduleDirectory: ["/node_modules/"],
+    coverageDirectory: "coverage",
+    moduleFileExtensions: ["js","json", "jsx"],
+    setupFilesAfterEnv: ["jest-enzyme"],
+    testEnvironment: "enzyme",
+    testEnvironmentOptions: {
+        "enzymeAdapter": "react16",
+      },
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    transform: {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.jsx$": "babel-jest",
+      },
+    verbose: true
+  };
+
